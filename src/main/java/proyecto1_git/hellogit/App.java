36167e036043a3973d89dea99f1fff03df76a873
alejandroupc <alejandroupc@alejandroupc-EasyNote-TE11HC>
2015-02-19 10:47:38 -0500

    Bon dia

---
+++ src/main/java/proyecto1_git/hellogit/App.java
@@ -0,0 +1,13 @@
+package proyecto1_git.hellogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
